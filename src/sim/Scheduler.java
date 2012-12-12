8c4ee2d5a5391f3fc35ba96abe9e0a9eb39cb748
Shen Li <shenli3@illinois.edu>
2012-12-12 14:43:59 -0600

    done

--- src/sim/Scheduler.java
+++ src/sim/Scheduler.java
@@ -2,6 +2,7 @@ package secon.sim;
 
 import secon.minerva.*;
 
+import java.util.Collections;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.io.BufferedReader;
@@ -12,6 +13,7 @@ public class Scheduler{
     
     private UserNodes userNodes = null;
     private Sink sink = null;
+    private String algo = null;
 
     //the file is currently under "/home/shenli/Dropbox/SECON2013/data/coor.txt"
     private String finPath = null;
@@ -20,7 +22,7 @@ public class Scheduler{
 
     private Random rand = null;
 
-    public Scheduler(String finPath, int simCnt){
+    public Scheduler(String finPath, int simCnt, String algo){
         this.sink = new Sink(Configure.SINK_LOC, Configure.SINK_RANGE, Configure.SINK_NUM);        
 
         this.userNodes = new UserNodes();
@@ -28,13 +30,99 @@ public class Scheduler{
         this.simCnt = simCnt;
         this.dim = Configure.DIM;
         this.rand = new Random();
+        this.algo = algo;
     }
 
     private int getRandPktCnt(){
         return rand.nextInt(Configure.MAX_PKT - Configure.MIN_PKT + 1) + Configure.MIN_PKT;
     }
 
-    private double syncData(UserNode user, Sink sink){
+
+    private void syncData(UserNode user, Sink sink){
+        if(this.algo.equals("minerva")){
+            doMinerva(user, sink);
+        }
+        else if(this.algo.equals("fifo")){
+            doFIFO(user, sink);
+        }
+        else if(this.algo.equals("random")){
+            doRandom(user, sink);
+        }
+        else if(this.algo.equals("distance")){
+            doDistance(user, sink);
+        }
+        else{
+            System.out.println("Unrecognized algorithm name");
+            assert(false);
+        }
+        
+    }
+
+    private void doDistance(UserNode user, Sink sink){
+        ArrayList<KAry> userData = user.getTrace();
+        ArrayList<KAry> sinkData = sink.getData();
+
+        ArrayList<KAry> order = Distance.orderComp(sinkData, userData);
+        int pktCnt = getRandPktCnt();
+
+        if(pktCnt < order.size()){
+            sink.appendData(new ArrayList(order.subList(0, pktCnt)));
+            user.updateTrace(new ArrayList(order.subList(pktCnt, order.size())));
+        }
+        else{
+            sink.appendData(order);
+            user.updateTrace(new ArrayList<KAry>());
+        }
+         
+    }
+
+    private void doRandom(UserNode user, Sink sink){
+        ArrayList<KAry> userData = user.getTrace();
+
+        Collections.shuffle(userData, new Random(System.nanoTime()));
+        int pktCnt = getRandPktCnt();
+        if(pktCnt < userData.size()){
+            sink.appendData(new ArrayList(userData.subList(0, pktCnt)));
+            user.updateTrace(new ArrayList(userData.subList(pktCnt, userData.size())));
+        }
+        else{
+            sink.appendData(userData);
+            user.updateTrace(new ArrayList());
+        }
+    }
+
+    private void doFIFO(UserNode user, Sink sink){
+        ArrayList<KAry> userData = user.getTrace();
+        int pktCnt = getRandPktCnt();
+        if(pktCnt < userData.size()){
+            sink.appendData(new ArrayList(userData.subList(0, pktCnt)));
+            user.updateTrace(new ArrayList(userData.subList(pktCnt, userData.size())));
+        }
+        else{
+            sink.appendData(userData);
+            user.updateTrace(new ArrayList());
+        }
+    }
+
+    private double calcScore(UserNode user, Sink sink){
+        //System.out.println("before ceate arraylist " + user + ", " + sink);
+        ArrayList<KAry> userData = user.getTrace();
+        ArrayList<KAry> sinkData = sink.getData();
+
+        //System.out.println("before gen user");
+        MetaGen userGen = new MetaGen(userData, Configure.INTERVAL, Configure.HI_RATIO);
+        MetaData userMeta = userGen.generate();
+
+        //System.out.println("before gen sink");
+        MetaGen sinkGen = new MetaGen(sinkData, Configure.INTERVAL, Configure.HI_RATIO);
+        MetaData sinkMeta = sinkGen.generate();
+
+        //System.out.println("before minerva");
+        Minerva m = new Minerva(sinkMeta, userMeta, sinkData, Configure.INTERVAL, Configure.HI_RATIO);
+        return m.coverageComp(sink.getData(), Configure.INTERVAL);
+    }
+
+    private void doMinerva(UserNode user, Sink sink){
         //System.out.println("before ceate arraylist " + user + ", " + sink);
         ArrayList<KAry> userData = user.getTrace();
         ArrayList<KAry> sinkData = sink.getData();
@@ -59,11 +147,12 @@ public class Scheduler{
             user.updateTrace(new ArrayList(order.subList(pktCnt, order.size())));
         }
         else{
-            sink.appendData(new ArrayList(order.subList(0, order.size())));
+            sink.appendData(order);
             user.updateTrace(new ArrayList<KAry>());
         }
 
-        return m.coverageComp(sink.getData(), Configure.INTERVAL);
+        //return m;
+        //return m.coverageComp(sink.getData(), Configure.INTERVAL);
     }
 
     public void simulate(){
@@ -80,6 +169,8 @@ public class Scheduler{
             br = new BufferedReader(fr);
             
             int t = -1;
+            int m = 0; //calculate the score every 10 minutes
+            double TEN_M = 600; //in s 
             int uid = -1;
             double lon = 0;
             double lat = 0;
@@ -105,7 +196,12 @@ public class Scheduler{
                 user.addOneTrace(kAry);
                 //System.out.println("before sync");                
                 if(this.sink.inRange(kAry)){
-                    System.out.println(t + ", " + cnt + ", " + lat + ", " + lon + ", " + syncData(user, sink));
+                    syncData(user, sink);
+                    if(t > TEN_M * (m + 1)){
+                        m = (int)(Math.floor(t / TEN_M ));
+                        System.out.println(t + ", " + cnt + ", " + lat + ", " + lon + ", " + calcScore(user, sink));                
+                    }
+                    //System.out.println(t + ", " + cnt + ", " + lat + ", " + lon + ", " + syncData(user, sink));
                 }
 
             }
@@ -116,15 +212,16 @@ public class Scheduler{
     }
 
     public static void main(String args[]){
-        if(args.length < 2){
-            System.out.println("Need 2 args: trace file location, and simulation round.");
+        if(args.length < 3){
+            System.out.println("Need 3 args: trace file location, simulation round, algorithm name (minerva/fifo/random/distance).");
             return;
         }
         //System.out.println(args[0] + ", " + args[1]);
         String path = args[0];
         int simCnt = Integer.parseInt(args[1]);
+        String algo = args[2];
 
-        Scheduler s = new Scheduler(path, simCnt);
+        Scheduler s = new Scheduler(path, simCnt, algo);
         s.simulate();        
     }
 }
