19f3270a2962ff9d9c4ad426fe4aae6aebc227fe
Shen Li <shenli3@tareka01.cs.uiuc.edu>
2012-12-12 21:21:00 -0600

    HI_RATIO set to 0.1

--- src/sim/Configure.java
+++ src/sim/Configure.java
@@ -12,7 +12,7 @@ public class Configure{
     //public static final int MIN_PKT = 20; //minimum possible number of data points sended during one transmission
     //public static final int MAX_PKT = 40;
     public static final double INTERVAL = 0.005;
-    public static final double HI_RATIO = 0.5;
+    public static final double HI_RATIO = 0.1;
     public static final double MIN_LAT = 39.5;
     public static final double MAX_LAT = 40.5;
     public static final double MIN_LON = 116;
